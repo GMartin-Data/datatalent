05fba2c45f2a5e985f3280a802d2116c6074dac3
ESNAULTQ <kent1.esnault@gmail.com>
2026-04-08 14:07:41 +0200

    feat(dbt): staging sirene

--- dbt/models/staging/sirene/stg_sirene__etablissements.sql
+++ dbt/models/staging/sirene/stg_sirene__etablissements.sql
@@ -1,3 +1,24 @@
--- TODO : voir dbt-stg-sirene-etablissements.md
--- Matérialisé en table (pas view) : 43M lignes raw, filtre à 17M.
 {{ config(materialized='table') }}
+
+WITH source AS (
+    SELECT *
+    FROM {{ source('sirene', 'sirene_etablissement') }}
+    WHERE etatAdministratifEtablissement = 'A'
+)
+
+SELECT
+    siret,
+    siren,
+    denominationUsuelleEtablissement        AS denomination_usuelle,
+    enseigne1Etablissement                  AS enseigne,
+    activitePrincipaleEtablissement         AS code_naf,
+    trancheEffectifsEtablissement           AS tranche_effectifs,
+    codeCommuneEtablissement                AS code_commune,
+    codePostalEtablissement                 AS code_postal,
+    libelleCommuneEtablissement             AS libelle_commune,
+    dateCreationEtablissement               AS date_creation,
+    etablissementSiege                      AS is_siege,
+    (statutDiffusionEtablissement = 'P')    AS is_diffusion_partielle,
+    (caractereEmployeurEtablissement = 'O') AS is_employeur
+
+FROM source
