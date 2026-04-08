2d9f17907192bb9650bedeca75757d9e373d6513
ESNAULTQ <kent1.esnault@gmail.com>
2026-04-08 16:29:24 +0200

    feat(dbt): staging sirene2

--- dbt/models/staging/sirene/stg_sirene__etablissements.sql
+++ dbt/models/staging/sirene/stg_sirene__etablissements.sql
@@ -7,15 +7,24 @@ WITH source AS (
 )
 
 SELECT
+    -- Identifiants
     siret,
     siren,
+
+    -- Dénomination
     denominationUsuelleEtablissement        AS denomination_usuelle,
     enseigne1Etablissement                  AS enseigne,
+
+    -- Activité et taille
     activitePrincipaleEtablissement         AS code_naf,
     trancheEffectifsEtablissement           AS tranche_effectifs,
+
+    -- Géographie
     codeCommuneEtablissement                AS code_commune,
     codePostalEtablissement                 AS code_postal,
     libelleCommuneEtablissement             AS libelle_commune,
+
+    -- Métadonnées
     dateCreationEtablissement               AS date_creation,
     etablissementSiege                      AS is_siege,
     (statutDiffusionEtablissement = 'P')    AS is_diffusion_partielle,
