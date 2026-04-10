80e257e5649f4616aa580352936946f47045f833
Greg Martin <gregory.martin.data@gmail.com>
2026-04-10 07:13:20 +0200

    feat(dbt): add staging models for geo source (regions, departements, communes)

--- dbt/models/staging/geo/stg_geo__departements.sql
+++ dbt/models/staging/geo/stg_geo__departements.sql
@@ -1,1 +1,17 @@
--- TODO : voir dbt-stg-geo.md
+WITH source AS (
+    SELECT * FROM {{ source('geo', 'geo_departements') }}
+),
+
+regions AS (
+    SELECT code, nom
+    FROM {{ ref('stg_geo__regions') }}
+)
+
+SELECT
+    source.code,
+    source.nom,
+    source.codeRegion   AS code_region,
+    regions.nom         AS nom_region,
+    source.zone         AS zone_geo
+FROM source
+LEFT JOIN regions ON source.codeRegion = regions.code
