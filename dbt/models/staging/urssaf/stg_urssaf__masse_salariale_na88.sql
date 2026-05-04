0067551fd4b087c793ac4fb68420d8ad9412ba1c
Abdel D <150450446+AbdelDpro@users.noreply.github.com>
2026-05-04 11:40:38 +0200

    feat: implémenter la couche de staging sous dbt pour urssaf masse sal… (#29)
    * feat: implémenter la couche de staging sous dbt pour urssaf masse salariale
    
    * fix: fusion scripts Louis & Abdel
    
    * fix: elimination d'un doublon
    
    * fix: corriger mélange de modeles

--- dbt/models/staging/urssaf/stg_urssaf__masse_salariale_na88.sql
+++ dbt/models/staging/urssaf/stg_urssaf__masse_salariale_na88.sql
@@ -1,1 +1,16 @@
 -- TODO : voir dbt-stg-urssaf-masse-salariale.md
+with source as (
+    select * from {{ source('urssaf', 'urssaf_masse_salariale') }}
+),
+renamed as (
+    select
+        code_na88,
+        libelle_na88,
+        annee as annee_date,
+        _ingestion_date as ingested_at,
+        masse_salariale_brute,
+        effectifs_salaries_moyens,
+        nb_etablissements
+    from source
+)
+select * from renamed
